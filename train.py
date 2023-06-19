44d15253b5d3797bcf2e892a89fe9879405556ab
Krishna Chaitanya Kosaraju <kkrishnachaitanya89@gmail.com>
2023-06-19 17:04:09 -0400

    fixed minor typos
    introduced self.head_size and used it consistently.

--- train.py
+++ train.py
@@ -6,36 +6,37 @@ from dataclasses import dataclass
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    vocab_size: int = 50304  # GPT-2 vocab_size of 50257, padded up to the nearest multiple of 64 for efficiency
     n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
+    n_head: int = 12 # number of attention heads 
+    n_embd: int = 768 # embedding size of the input
     dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    bias: bool = True  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class MultiHeadAttention(layers.Layer):
     def __init__(self, config):
         super(MultiHeadAttention, self).__init__()
 
-        self.num_heads = config.num_heads
-        self.head_size = config.head_size
+        self.num_heads = config.n_head
+        self.head_size = config.n_embd // config.n_head
 
-        # projecting  input into key, query and value for all attentian heads,but in batchw
-        self.c_attn = layers.Dense(3 * config.head_size, use_bias=False)
-        
-        # regularization
+        # Projecting input into key, query, and value for all attention heads, but in batch
+        self.c_attn = layers.Dense(3 * config.n_embd, use_bias=config.bias)
+
+        # Regularization
         self.attn_dropout = layers.Dropout(config.dropout)
         self.resid_dropout = layers.Dropout(config.dropout)
+    
     def call(self, x):
         B, T, C = x.shape
 
-        # Linear transformation for queries, keys, and values
-        qkv = self.c_attn(x)  # Input shape: (B, T, C), Output shape: (B, T, 3 * head_size)
+        # Linear transformation for queries, keys, and values, note that C = n_embd
+        qkv = self.c_attn(x)  # Input shape: (B, T, C), Output shape: (B, T, 3 * n_embd)
 
         # Split the queries, keys, and values
-        q, k, v = tf.split(qkv, 3, axis=-1)  # Input shape: (B, T, 3 * head_size), Output shapes: 3 * (B, T, head_size)
+        q, k, v = tf.split(qkv, 3, axis=-1)  # Input shape: (B, T, 3 * n_embd), Output shapes: 3 * (B, T, n_embd)
 
-        # Reshape queries, keys, and values for multi-head attention with a  -  head_size = C//num_heads
+        # Reshape queries, keys, and values for multi-head attention with head_size = n_embd // num_heads
         q = tf.reshape(q, (B, T, self.num_heads, -1))  # Output shape: (B, T, num_heads, head_size)
         k = tf.reshape(k, (B, T, self.num_heads, -1))  # Output shape: (B, T, num_heads, head_size)
         v = tf.reshape(v, (B, T, self.num_heads, -1))  # Output shape: (B, T, num_heads, head_size)
@@ -48,29 +49,29 @@ class MultiHeadAttention(layers.Layer):
         v = tf.transpose(v, perm=[0, 2, 1, 3])  # Output shape: (B, num_heads, T, head_size)
 
         # Compute attention scores ("affinities")
-        wei = tf.matmul(q, tf.transpose(k, perm=[0, 1, 3, 2])) * (self.head_size ** -0.5)  # Output shape: (B, num_heads, T, T)
+        wei = tf.matmul(q, k, transpose_b=True) * (self.head_size ** -0.5)  # Output shape: (B, num_heads, T, T)
 
         mask = tf.linalg.band_part(tf.ones_like(wei), -1, 0)  # Lower triangular matrix of ones
         wei = tf.where(mask == 1, wei, float("-inf"))  # Set upper triangular part to -inf
 
         wei = tf.nn.softmax(wei, axis=-1)  # Output shape: (B, num_heads, T, T)
-        wei = self.attn_dropout(wei) # regularization step 1
+        wei = self.attn_dropout(wei)  # Regularization step 1
+
         # Perform the weighted aggregation of the values
         out = tf.matmul(wei, v)  # Output shape: (B, num_heads, T, head_size)
 
         # Transpose and reshape the output to match the original shape
         out = tf.transpose(out, perm=[0, 2, 1, 3])  # Output shape: (B, T, num_heads, head_size)
-        out = tf.reshape(out, (B, T, -1))  # Output shape: (B, T, C) # note that C = num_heads*head_size
-        out = self.resid_dropout(out) # regularization step 2
+        out = tf.reshape(out, (B, T, -1))  # Output shape: (B, T, C) - note that C = num_heads * head_size = n_embd
+        out = self.resid_dropout(out)  # Regularization step 2  - input and output of the NN are of same dimensions
         return out
 
 class MLP(layers.Layer):
     def __init__(self, config):
-        super().__init__()
-        n_embed = config.n_embed
-        self.c_fc = layers.Dense(4* config.n_embed, use_bias=config.bias, 
-                                 activation=tf.keras.activations.gelu)
-        self.c_proj  = layers.Dense(config.n_embd, use_bias=config.bias)
+        super(MLP, self).__init__()
+        n_embed = config.n_embd
+        self.c_fc = layers.Dense(4 * n_embed, use_bias=config.bias, activation=tf.keras.activations.gelu)
+        self.c_proj = layers.Dense(config.n_embd, use_bias=config.bias)
         self.dropout = layers.Dropout(config.dropout)
 
     def call(self, x):
